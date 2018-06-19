fef52175e8706fb79e97b1b1b54e50b9ab15d763
oWlogona <artemdanilenko1997@gmail.com>
2018-06-20 00:03:05 +0300

    problem_with_chating

--- send_message/models.py
+++ send_message/models.py
@@ -3,22 +3,23 @@ from django.contrib.auth.models import User
 
 
 class Dialog(models.Model):
-    user_1 = models.OneToOneField(
+    user_1 = models.ForeignKey(
         User, blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='user_1')
-    user_2 = models.OneToOneField(
+    user_2 = models.ForeignKey(
         User, blank=True, null=True, default=None, on_delete=models.CASCADE, related_name='user_2')
 
     def __str__(self):
-        return self.user_2
+        return self.user_2.username
 
 
 class Message(models.Model):
-    sender = models.CharField(max_length=128)
+    sender = models.ForeignKey(User, blank=True, null=True, default=None, on_delete=models.CASCADE)
     text_message = models.CharField(
         max_length=255, blank=True, null=True, default=None)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
     dialog = models.ForeignKey(
         Dialog, blank=True, null=True, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.sender
+        return self.sender.username
--- send_message/views.py
+++ send_message/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Dialog, Message
+from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 
@@ -7,13 +8,14 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def create_dialog(request, user_id):
     if request.user.is_authenticated:
-        dialog_id = Dialog.objects.filter(
-            user_1=request.user.id, user_2=user_id)
-        if not dialog:
-            dialog_id = Dialog.objects.create(
-                user_1=request.user.id, user_2=user_id)
-            url = '/dialog/' + str(dialog_id) + '/'
-            return HttpResponseRedirect(url)
+        user_1 = User.objects.get(id=request.user.id)
+        user_2 = User.objects.get(id=user_id)
+        dialog_id = Dialog.objects.filter(user_1=user_1, user_2=user_2)
+        if not dialog_id:
+            dialog_id = Dialog.objects.create(user_1=user_1, user_2=user_2)
+
+        dialog_id = Dialog.objects.get(user_1=user_1, user_2=user_2)
+        dialog_id = dialog_id.id
         url = '/dialog/' + str(dialog_id) + '/'
         return HttpResponseRedirect(url)
     else:
@@ -23,5 +25,16 @@ def create_dialog(request, user_id):
 
 @login_required
 def dialog(request, dialog_id):
-    pass
-    
+    if request.user.is_authenticated:
+        messages = Message.objects.all()
+        print(messages)
+        if request.method == 'POST':
+            sender = request.user
+            data_text = request.POST['message']
+            Message.objects.create(sender=sender, text_message=data_text, dialog=dialog_id)
+            dialog_id = Message.objects.get(dialog=dialog_id)
+            dialog_id = dialog_id.id
+            return render(request, 'room_page.html', locals())
+    else:
+        print('read again')
+    return render(request, 'room_page.html', locals())
